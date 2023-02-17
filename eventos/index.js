3c27bb5c435f9e0652d7cbae6ed326bc47daef5b
mafeguimaraes <mariafefe201@gmail.com>
2023-02-17 09:22:10 -0300

    Events in HTML

---
+++ eventos/index.js
@@ -0,0 +1,19 @@
+function mouseEnterEvent() {
+  console.log("mouse entered");
+}
+
+function changeEvent() {
+  console.log("value changed");
+}
+
+function focusEvent() {
+  console.log("gained focus");
+}
+
+function blurEvent() {
+  console.log("lost focus");
+}
+
+function clickEvent() {
+  alert("clicked");
+}
