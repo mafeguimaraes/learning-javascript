6cd8818d5435778690d46e6e1a4353df6314055e
mafeguimaraes <mariafefe201@gmail.com>
2023-04-03 14:30:45 -0300

    usando webpack

---
+++ usando-webpack/src/index.js
@@ -0,0 +1,4 @@
+import dayjs from "dayjs";
+import "./styles/index.css";
+
+alert(`Hoje é: ${dayjs().format("DD/MM/YYYY")}`);
---
+++ usando-webpack/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
